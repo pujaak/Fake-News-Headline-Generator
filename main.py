5ddca54b403483e7a52e43bf64d9244099949ebf
Puja <itispujakumari21@gmail.com>
2025-12-05 14:25:27 +0530

    Add basic main functionality for generating fake news headlines

---
+++ main.py
@@ -0,0 +1,14 @@
+# subject: who / what
+# action: e.g declares, rides, spotted etc.
+# places/ objects : where / with whom or with what
+# lets keep it bollywood
+import random
+subjects_list = ['SRK', 'Salman Khan', 'Kajol', 'Alia Bhatt', 'Katrina Kaif', 'Ranbir Kapoor', 'Vicky Kaushal', 'Kriti Sanon' ]
+actions_list = ['spotted juggling NCERT books', 'dancing Tango blindfold', 'ate Chicken Noodle Soup with Soda on the side', 'wore Doraemon print shirt', 'riding Buffalo', 'crossed bridge on one leg']
+places_list = ['at the India Gate', 'near the pollution control house', 'between a Cherry Blosson Tree and a Palas Tree', 'over the train', 'near Red Fort']
+
+subject = random.choice(subjects_list)
+action = random.choice(actions_list)
+place = random.choice(places_list)
+
+print(f"{subject} {action} {place}")
